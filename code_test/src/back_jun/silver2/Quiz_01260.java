d2ae46875b23eb8122ca6d40183c7d2a6ac5ee4b
Mun Kyoung-min <mun9659@naver.com>
2022-10-16 01:23:16 +0900

    2022-10-16 SILVER2 1문제 수정

--- code_test/src/back_jun/silver2/Quiz_01260.java
+++ code_test/src/back_jun/silver2/Quiz_01260.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 // DFS와 BFS
 public class Quiz_01260 {
 	static Queue<Integer> que = new LinkedList<>();
+	static Stack<Integer> stack = new Stack<>();
 	static int n, m, v;
 	static int[][] node;
 	static boolean[] visited;
@@ -32,7 +34,7 @@ public class Quiz_01260 {
 			node[k][s]++;
 		}
 		
-		dfs(v);
+		dfs2(v);
 		sb.append("\n");
 		
 		visited = new boolean[n+1];
@@ -52,6 +54,30 @@ public class Quiz_01260 {
 			if((node[v][i] == 1 || node[i][v] == 1) && !visited[i]) {
 				dfs(i);
 			}
+		}
+	}
+	
+	// 스택 방식
+	static void dfs2(int v) {
+		stack.push(v);
+		visited[v] = true;
+		
+		sb.append(v).append(" ");
+		
+		while(!stack.isEmpty()) {
+			int k = stack.peek();
+			
+			boolean hasNearNode = false;
+			for(int i = 1; i <= n; i++) {
+				if((node[k][i] == 1 || node[i][k] == 1) && !visited[i]) {
+					hasNearNode = true;
+					visited[i] = true;
+					stack.push(i);
+					sb.append(i).append(" ");
+					break;
+				}
+			}
+			if(!hasNearNode) stack.pop();
 		}
 	}
 	
