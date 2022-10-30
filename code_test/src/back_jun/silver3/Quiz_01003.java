36a7ab4efc3546c256c98d63fc3e8a8a8d5a435e
Mun Kyoung-min <mun9659@naver.com>
2022-10-30 22:05:45 +0900

    2022-10-30 SILVER3 복기

--- code_test/src/back_jun/silver3/Quiz_01003.java
+++ code_test/src/back_jun/silver3/Quiz_01003.java
@@ -5,29 +5,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 // 피보나치 함수
-// 시간 제한 0.25초
-// 거의 다 해놓고 chk[1][0], chk[1][1] 오타 + return 값을 chk배열로 해서 답을 봄....
+// 다이나믹 프로그래밍
 public class Quiz_01003 {
-	static int[][] chk = new int[41][2];
+	public static Integer[][] dp = new Integer[41][2];
 	public static void main(String[] args) throws IOException  {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		chk[0][0] = 1;
-		chk[0][1] = 0;
-		chk[1][0] = 0;
-		chk[1][1] = 1;
 		int T = Integer.parseInt(br.readLine());
+		// 0 / 1 의 개수 초기값
+		dp[0][0] = 1;
+		dp[0][1] = 0;
+		dp[1][0] = 0;
+		dp[1][1] = 1;
 		for(int i = 0; i < T; i++) {
 			int n = Integer.parseInt(br.readLine());
-			fib(n);
-			System.out.printf("%d %d\n", chk[n][0], chk[n][1]);
+			Integer[] chk = fib(n);
+			System.out.println(chk[0] + " " + chk[1]);
 		}
 	}
 	
-	static int[] fib(int n) {
-		if(chk[n][0] == 0 && chk[n][1] == 0) {
-			chk[n][0] = fib(n-1)[0] + fib(n-2)[0];
-			chk[n][1] = fib(n-1)[1] + fib(n-2)[1];
+	public static Integer[] fib(int n) {
+		if(dp[n][0] == null && dp[n][1] == null) {
+			dp[n][0] = fib(n - 1)[0] + fib(n - 2)[0];
+			dp[n][1] = fib(n - 1)[1] + fib(n - 2)[1];
 		}
-		return chk[n];
+		return dp[n];
 	}
 }
--- code_test/src/back_jun/silver3/Quiz_02606.java
+++ code_test/src/back_jun/silver3/Quiz_02606.java
@@ -7,72 +7,68 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 // 바이러스
-// 연습 많이 하자
+// BFS, DFS
+// 다시 풀어보자
 public class Quiz_02606 {
-	static int cnt = 0;
-	// static int[][] com; // 재귀함수 전용
-	// static boolean[] visited; // 재귀함수 전용
+	public static int cnt = 0;
+	public static int[][] arr;
+	public static boolean[] visited;
 	public static void main(String[] args) throws IOException  {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
 		int n = Integer.parseInt(br.readLine());
 		int twin = Integer.parseInt(br.readLine());
-		int[][] com = new int[n+1][n+1];
-		boolean[] visited = new boolean[n+1];
 		
-		StringTokenizer st;
+		arr = new int[n+1][n+1];
+		visited = new boolean[n+1];
+		
 		for(int i = 0; i < twin; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int t1 = Integer.parseInt(st.nextToken());
-			int t2 = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
 			
-			com[t1][t2]++;
+			int n1 = Integer.parseInt(st.nextToken());
+			int n2 = Integer.parseInt(st.nextToken());
+			arr[n1][n2]++;
 		}
 		
-		// dfs(1);
-		dfs2(com, 1, visited);
+		// dfs1(1);
+		dfs2(1);
 		
 		System.out.println(cnt);
 	}
 	
-	// 재귀함수 표현
-	/*
-	static void dfs(int n) {
-		// 현재 노드 방문 처리
-		visited[n] = true;
-		cnt++;
-		// 접속한 노드
-		// System.out.println(n);
+	// 재귀 방식
+	public static void dfs1(int start) {
+		visited[start] = true;
 		
-		for(int i = 0; i < com[n].length; i++) {
-			if(visited[i]==false && (com[n][i] == 1 || com[i][n] == 1)) {
-				dfs(i);
+		for(int i = 1; i < arr.length; i++) {
+			if(!visited[i] && (arr[start][i] == 1 || arr[i][start] == 1)) {
+				cnt++;
+				dfs1(i);
 			}
 		}
 	}
-	*/
-	// 스택 표현
-	static void dfs2(int[][] com, int start, boolean[] visited) {
+	
+	// 스택 방식
+	public static void dfs2(int start) {
 		visited[start] = true;
-		System.out.println(start);
 		
 		Stack<Integer> stack = new Stack<>();
 		stack.push(start);
-		
 		while(!stack.isEmpty()) {
-			int now = stack.peek();
-			
-			boolean hasNearNode = false; // 방문하지 않은 인접 노드가 있는지 확인
-			for(int i = 0; i < com[now].length; i++) {
-				if(!visited[i] && (com[now][i] == 1 || com[i][now] == 1)) {
+			int value = stack.peek();
+			boolean hasNearNode = false; // 근접 노드 확인용
+			for(int i = 1; i < arr.length; i++) {
+				if(!visited[i] && (arr[value][i] == 1 || arr[i][value] == 1)) {
+					System.out.println("i : " + i);
 					hasNearNode = true;
 					stack.push(i);
-					visited[i] = true;
-					System.out.println(i);
 					cnt++;
+					dfs2(i);
 					break;
 				}
 			}
-			if(hasNearNode == false) stack.pop();
+			if(!hasNearNode) stack.pop();
 		}
 	}
 }
