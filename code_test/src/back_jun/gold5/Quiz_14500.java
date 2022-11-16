5ffb28c4693e6e88697733e7dd4b8c4973129c64
Mun Kyoung-min <mun9659@naver.com>
2022-11-16 12:57:44 +0900

    2022-11-16 GOLD5 1문제

---
+++ code_test/src/back_jun/gold5/Quiz_14500.java
@@ -0,0 +1,16 @@
+package back_jun.gold5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 테트로미노
+// 거의 다 풀긴 했지만 역시 가지뻗기는 조금 어렵다. 다시 풀자!
+public class Quiz_14500 {
+	public static void main(String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+	}
+}
