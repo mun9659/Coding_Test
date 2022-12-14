c575904f39bd0361d222d70ea20631c039c30654
Mun Kyoung-min <mun9659@naver.com>
2022-12-14 11:10:37 +0900

    2022-12-14 SILVER1,2 2문제 (DP, 복기)

---
+++ code_test/src/back_jun/silver1/Quiz_09465.java
@@ -0,0 +1,40 @@
+package back_jun.silver1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 스티커
+// 답을 봤지만 이해는 됐다. 다시 풀어보기
+public class Quiz_09465 {
+	public static void main(String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= t; tc++) {
+			int n = Integer.parseInt(br.readLine());
+			
+			int[][] arr = new int[2][n + 1];
+			int[][] dp = new int[2][n + 1];
+			for(int i = 0; i < 2; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 1; j <= n; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			dp[0][1] = arr[0][1];
+			dp[1][1] = arr[1][1];
+			for(int i = 2; i <= n; i++) {
+				dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
+				dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
+			}
+			
+			System.out.println(Math.max(dp[0][n], dp[1][n]));
+		}
+	}
+}
--- code_test/src/back_jun/silver2/Quiz_21317.java
+++ code_test/src/back_jun/silver2/Quiz_21317.java
@@ -9,39 +9,43 @@ import java.util.StringTokenizer;
 // DP 보다는 DFS가 더 강해서 조금 헷갈렸음, 다시 풀어보기
 public class Quiz_21317 {
 	
-	public static int[][] jumps;
 	public static int n, k, result = Integer.MAX_VALUE;
+	public static int[][] jumps;
 	
 	public static void main(String[] args) throws IOException  {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		n = Integer.parseInt(br.readLine());
-		jumps = new int[n - 1][2];
-		for(int i = 0; i < n - 1; i++) {
+		jumps = new int[n][2];
+		for(int i = 1; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
+			
 			jumps[i][0] = Integer.parseInt(st.nextToken());
 			jumps[i][1] = Integer.parseInt(st.nextToken());
 		}
 		k = Integer.parseInt(br.readLine());
 		
-		dfs(0, 0, false);
+		dfs(1, 0, false);
 		
 		System.out.println(result);
 	}
 	
-	public static void dfs(int stoneidx, int energy, boolean bigJump) {
-		if(stoneidx == n - 1) {
-			result = Math.min(result, energy);
+	public static void dfs(int idx, int energy, boolean check) {
+		if(idx == n) {
+			result = Math.min(energy, result);
 			return;
 		}
-		if(stoneidx > n - 1) return;
+		
+		if(idx > n) return;
 		
 		// 1. 작은 점프
-		dfs(stoneidx + 1, energy + jumps[stoneidx][0], bigJump);
+		dfs(idx + 1, energy + jumps[idx][0], check);
 		// 2. 큰 점프
-		dfs(stoneidx + 2, energy + jumps[stoneidx][1], bigJump);
+		dfs(idx + 2, energy + jumps[idx][1], check);
 		// 3. 매우 큰 점프
-		if(!bigJump) dfs(stoneidx + 3, energy + k, true);
+		if(!check) {
+			dfs(idx + 3, energy + k, true);
+		}
 	}
 }
