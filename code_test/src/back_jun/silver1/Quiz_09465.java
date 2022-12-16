b815c86fec88f5f4e333cefb04f85d9dfa99693f
Mun Kyoung-min <mun9659@naver.com>
2022-12-16 13:48:22 +0900

    2022-12-16 SILVER1 1문제 (DP 복기)

--- code_test/src/back_jun/silver1/Quiz_09465.java
+++ code_test/src/back_jun/silver1/Quiz_09465.java
@@ -22,16 +22,17 @@ public class Quiz_09465 {
 			int[][] dp = new int[2][n + 1];
 			for(int i = 0; i < 2; i++) {
 				st = new StringTokenizer(br.readLine());
-				for(int j = 1; j <= n; j++) {
+				for(int j = 1; j < n + 1; j++) {
 					arr[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
 			dp[0][1] = arr[0][1];
 			dp[1][1] = arr[1][1];
-			for(int i = 2; i <= n; i++) {
-				dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
-				dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
+			
+			for(int i = 2; i < n + 1; i++) {
+				dp[0][i] = Math.max(dp[1][i - 2], dp[1][i - 1]) + arr[0][i];
+				dp[1][i] = Math.max(dp[0][i - 2], dp[0][i - 1]) + arr[1][i];
 			}
 			
 			System.out.println(Math.max(dp[0][n], dp[1][n]));
